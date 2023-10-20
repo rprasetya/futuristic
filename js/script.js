813eb75c9904f3370f6af002f1fa0dc987c5019e
rprasetya <rayaprasetya90@gmail.com>
2023-10-20 11:41:44 +0800

    futuristic-si-web

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navigationList = document.querySelectorAll('.navigation')
+
+navigationList.forEach(navigation => {
+    navigation.style.opacity = '0';
+    navigation.style.transform = 'translateX(100vw)';
+});
+
+const delay = 50
+
+hamburgerMenu.addEventListener('click', () => {
+    navigationList.forEach((navigation, index) => {
+        if (navigation.style.opacity === '0') {
+            navigation.style.transitionDelay = `${delay * index}ms`;
+            navigation.style.opacity = '1'
+            navigation.style.transform = 'translateX(0vw)'
+        } else {        
+        navigation.style.opacity = '0'
+        navigation.style.transform = 'translateX(100vw)'
+
+        }
+    });
+});
+
+const taglines = document.querySelectorAll('.tagline')
+window.addEventListener('load', () => {
+    taglines.forEach(tagline =>{
+        tagline.style.opacity = '1'
+        tagline.style.transform = 'translateX(0vw)'
+    })
+})
